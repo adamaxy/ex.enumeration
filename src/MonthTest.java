9ac694c50565a3039e4c6746b0c7521977276e2a
kira <kira@MacBook-Air-di-kira.local>
2023-09-06 18:06:11 +0200

    ok

---
+++ src/MonthTest.java
@@ -0,0 +1,8 @@
+public class MonthTest {
+        public static void main(String[] args) {
+            for (Month month : Month.values()) {
+                if (month.toString().endsWith("Y")) {
+                    System.out.println(month + " ends with Y");
+                } else {
+                    System.out.println(month + " doesn't end with Y");
+                }}}}
