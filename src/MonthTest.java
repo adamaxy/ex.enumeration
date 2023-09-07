b63e197a83317010384dd96b5b20f414b9a18d6a
kira <kira@MacBook-Air-di-kira.local>
2023-09-07 18:13:37 +0200

    ho formattato il codice

--- src/MonthTest.java
+++ src/MonthTest.java
@@ -1,8 +1,11 @@
 public class MonthTest {
-        public static void main(String[] args) {
-            for (Month month : Month.values()) {
-                if (month.toString().endsWith("Y")) {
-                    System.out.println(month + " ends with Y");
-                } else {
-                    System.out.println(month + " doesn't end with Y");
-                }}}}
+    public static void main(String[] args) {
+        for (Month month : Month.values()) {
+            if ( month.toString().endsWith("Y") ) {
+                System.out.println(month + " ends with Y");
+            } else {
+                System.out.println(month + " doesn't end with Y");
+            }
+        }
+    }
+}
